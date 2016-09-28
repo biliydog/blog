cf24c1ad6dfebf34d8895d4c7805622eb0d2c939
USER-20150330GW\Administrator <tiandick@126.com>
2016-09-28 16:47:17 +0800

    删掉注释

--- blog/blogserver.py
+++ blog/blogserver.py
@@ -13,14 +13,10 @@ app.register_blueprint(sbapi_routes, url_prefix='/api')
 
 
 if __name__ == '__main__':
-    # debug 模式可以自动加载你对代码的变动, 所以不用重启程序
-    # host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码
     config = dict(
         debug=True,
         host='0.0.0.0',
         port=3000,
     )
     app.run(**config)
-    # app.run() 开始运行服务器
-    # 所以你访问下面的网址就可以打开网站了
-    # http://127.0.0.1:3000/
+   
