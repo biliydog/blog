3fe6cb63eaa4e2abcae8faf82a768c2517ca3eae
USER-20150330GW\Administrator <tiandick@126.com>
2016-10-15 14:18:53 +0800

    啊啊

--- blog/routes/article.py
+++ blog/routes/article.py
@@ -31,7 +31,7 @@ def all_article():
     return render_template('store.html', articles=list)
 
 
-@article.route('/edit_view' , methods=['POST'])
+@article.route('/edit_view')
 def edit_view():
     return render_template('edit_article.html')
 
